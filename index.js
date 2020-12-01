a3d40a49a7a0f5ad423be3a746f08f15fc219531
Sebastián Moreno <morenosebastian117@gmail.com>
2020-12-01 11:09:59 +0000

    Primera versión del módulo de matemáticas básica OK

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    /**
+     * Suma de dos números
+     * @example
+     * n1 = 1, n2 = 3 = -> resultado 4
+     * @param {*} n1 Número uno de la suma
+     * @param {*} n2 Número dos de la suma
+     */
+    suma: function (n1, n2) {
+        return n1 + n2;
+    },
+    /**
+     * Resta de dos números
+     * @example
+     * n1 = 3, n2 = 1 = -> resultado 2
+     * @param {*} n1 Número uno de la resta
+     * @param {*} n2 Número dos de la resta
+     */
+    resta: function (n1, n2) {
+        return n1 - n2;
+    },
+    /**
+     * División de dos números
+     * @example
+     * n1 = 4, n2 = 2 = -> resultado 2
+     * @param {*} n1 Número uno de la división
+     * @param {*} n2 Número dos de la división
+     */
+    division: function (n1, n2) {
+        return n1 / n2;
+    },
+    /**
+     * Multiplicación de dos números
+     * @example
+     * n1 = 3, n2 = 4 = -> resultado 12
+     * @param {*} n1 Número uno de la multiplicación
+     * @param {*} n2 Número dos de la multiplicación
+     */
+    multiplicacion: function (n1, n2) {
+        return n1 * n2;
+    }
+}
---
+++ pruebas.js
@@ -0,0 +1,6 @@
+const m = require('.');
+
+console.log(m.suma(1, 2));
+console.log(m.resta(11, 24));
+console.log(m.division(1, 2));
+console.log(m.multiplicacion(1, 2));
