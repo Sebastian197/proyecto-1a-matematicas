e18a6ee4d54af2ee93150e6e66d12214c1e4c1e1
Sebastián Moreno <morenosebastian117@gmail.com>
2020-12-01 16:58:13 +0000

    Funcionalidad para validar números

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
      * @param {*} n2 Número dos de la suma
      */
     suma: function (n1, n2) {
-        return n1 + n2;
+        return (this.esNumero(n1, n2) ? n1 + n2 : this.mensajeError());
     },
     /**
      * Resta de dos números
@@ -17,7 +17,7 @@ module.exports = {
      * @param {*} n2 Número dos de la resta
      */
     resta: function (n1, n2) {
-        return n1 - n2;
+        return (this.esNumero(n1, n2) ? n1 - n2 : this.mensajeError());
     },
     /**
      * División de dos números
@@ -27,7 +27,7 @@ module.exports = {
      * @param {*} n2 Número dos de la división
      */
     division: function (n1, n2) {
-        return n1 / n2;
+        return (this.esNumero(n1, n2) ? n1 / n2 : this.mensajeError());
     },
     /**
      * Multiplicación de dos números
@@ -37,6 +37,23 @@ module.exports = {
      * @param {*} n2 Número dos de la multiplicación
      */
     multiplicacion: function (n1, n2) {
-        return n1 * n2;
+        return (this.esNumero(n1, n2) ? n1 * n2 : this.mensajeError());
+    },
+    /**
+     * Lanza por consola un mensaje de error cuando no tengamos valores númericos
+     */
+    mensajeError: function () {
+        console.log('Un valor o los dos valores no son númericos');
+    },
+    /**
+     * Comprueba si los parámetros pasados son números
+     * * @example
+     * n1 = true, n2 = 4 = -> resultado 'Un valor o los dos valores no son númericos'
+     * @param {*} n1 Número uno
+     * @param {*} n2 Número dos
+     */
+    esNumero: function (n1, n2) {
+        if (typeof n1 !== 'number' || typeof n2 !== 'number') return false;
+        return true;
     }
 }
--- pruebas.js
+++ pruebas.js
@@ -4,3 +4,4 @@ console.log(m.suma(1, 2));
 console.log(m.resta(11, 24));
 console.log(m.division(1, 2));
 console.log(m.multiplicacion(1, 2));
+console.log(m.multiplicacion('1', 'gfad'));
